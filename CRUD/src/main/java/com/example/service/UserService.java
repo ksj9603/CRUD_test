381105ec01e1e66aa6a449a59b0b4eaa97101bfe
khs <khs@DESKTOP-58JPHNH>
2022-11-02 16:21:18 +0900

    CRUD 1

---
+++ CRUD/src/main/java/com/example/service/UserService.java
@@ -0,0 +1,60 @@
+package com.example.service;
+
+import java.sql.Connection;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.sql.DataSource;
+import javax.transaction.Transactional;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.mapper.PostMapper;
+import com.example.vo.UserVO;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+//@RequiredArgsConstructor
+public class UserService {
+
+//	@Autowired
+//	private final DataSource dataSource;
+//	
+//	@Autowired
+//	private final JdbcTemplate jdbcTemplate;
+//	
+//	@Autowired
+//	private SqlSessionFactory sqlSessionFactory;
+	
+	@Autowired
+	PostMapper postMapper;
+	
+	
+	@Transactional
+	public void save(UserVO userVO) {
+//		try(Connection connection = sqlSessionFactory.openSession().getConnection()) {
+			
+			
+			System.out.println("세션성공");
+//			jdbcTemplate.execute("insert into account values('3','3','3')");
+			postMapper.save(userVO);
+			
+			
+//			String id = userVO.getId();
+//			String pwd = userVO.getPwd();
+//			String name = userVO.getName();
+//			System.out.println(connection.getMetaData().getUserName());
+//			System.out.println(connection.getMetaData().getURL());
+//			System.out.println(dataSource.getClass());
+//			System.out.println(id+pwd+name);
+//			jdbcTemplate.execute("insert into account values('"+id+"','"+pwd+"','"+name+"')");
+		}
+		
+	}
+	
+
